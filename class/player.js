a41ad902d4d6be3a967756e729e5b90358d4315a
kishan2624 <ftd10822422@dseu.ac.in>
2024-05-11 16:07:38 +0530

    crt class player

--- class/player.js
+++ class/player.js
@@ -1,7 +1,24 @@
-class Player{
+export class Player{
     constructor(game){
         this.game = game;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = 0;
+        this.y = this.game.height - this.height;
+        this.image = document.getElementById("player")
+        this.imagePosition = {
+            x:0,
+            y:0,
+        }
+        this.imageWidth = 575;
+        this.imageHeight = 523;
     }
-    update(){}
-    draw(){}
+
+    update(){
+       
+    }
+
+    draw(context){
+        context.drawImage(this.image,this.imagePosition.x,this.imagePosition.y,this.imageWidth,this.imageHeight,this.x,this.y,this.width,this.height)
+    }
 }
